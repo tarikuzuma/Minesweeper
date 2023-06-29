6e7ab3e38a74aa7b6ccd610423b2eed12b46eebf
Tarikuzuma <edwinjrgumba@gmail.com>
2023-06-29 22:31:23 +0800

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module minesweeper {
+	//My small and practice rendition of Minesweeper for Java.
+	/*
+	 * I specifically did this to piss off some engineering students since it took them 2 months to create
+	 * a simple java game. If you're reading this and you're from APC, I love you.
+	 */
+}
